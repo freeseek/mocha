b956a1da190c19863c7f4783b62ac09586b6956d
Giulio Genovese <giulio.genovese@gmail.com>
2019-02-18 23:56:30 -0500

    several updates

--- plot_summary.R
+++ plot_summary.R
@@ -2,7 +2,7 @@
 ###
 #  The MIT License
 #
-#  Copyright (C) 2017-2018 Giulio Genovese
+#  Copyright (C) 2017-2019 Giulio Genovese
 #
 #  Author: Giulio Genovese <giulio.genovese@gmail.com>
 #
@@ -25,15 +25,21 @@
 #  THE SOFTWARE.
 ###
 
+suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(ggplot2))
 
-args <- commandArgs(trailingOnly = TRUE)
-out_pdf <- args[1]
-stats <- args[2]
-mocha <- args[3]
+parser <- ArgumentParser(description='Plot MoChA summary statistics (version 2019-02-15)')
+parser$add_argument('--stats', metavar = '<file.tsv>', type = 'character', required = TRUE, help = 'input MoChA stats file')
+parser$add_argument('--calls', metavar = '<file.tsv>', type = 'character', required = TRUE, help = 'input MoChA calls file')
+parser$add_argument('--pdf', metavar = '<file.pdf>', type = 'character', help = 'output PDF file')
+parser$add_argument('--fontsize', metavar = '<integer>', type = 'integer', default = 16, help = 'font size [16]')
+args <- parser$parse_args(commandArgs(trailingOnly = TRUE))
 
-df_stats <- read.table(stats, sep = '\t', header = TRUE)
-df <- read.table(mocha, sep = '\t', header = TRUE)
+df_stats <- read.table(args$stats, sep = '\t', header = TRUE)
+df_calls <- read.table(args$calls, sep = '\t', header = TRUE)
+df_calls$CHROM <- as.factor(gsub('^chr', '', gsub('^chrM', 'MT', df_calls$CHROM)))
+ord <- order(as.numeric(gsub('MT', '26', gsub('Y', '24', gsub('X', '23', levels(df_calls$CHROM))))))
+df_calls$CHROM <- factor(df_calls$CHROM, levels(df_calls$CHROM)[ord])
 
 if ('LRR_MEDIAN' %in% colnames(df_stats)) {
   model <- 'array'
@@ -42,143 +48,78 @@ if ('LRR_MEDIAN' %in% colnames(df_stats)) {
   df_stats$MT_ADJ_LRR_MEDIAN <- df_stats$MT_LRR_MEDIAN - df_stats$LRR_MEDIAN
 } else {
   model <- 'wgs'
-  # df$LDEV <- log(df$REL_COV/2)
   df_stats$X_NONPAR_ADJ_LRR_MEDIAN <- log(df_stats$X_NONPAR_COV_MEDIAN) - log(df_stats$COV_MEDIAN)
   df_stats$Y_NONPAR_ADJ_LRR_MEDIAN <- log(df_stats$Y_NONPAR_COV_MEDIAN) - log(df_stats$COV_MEDIAN)
   df_stats$MT_ADJ_LRR_MEDIAN <- log(df_stats$MT_COV_MEDIAN) - log(df_stats$COV_MEDIAN)
 }
 
-idx <- !( df$CHROM %in% c('X', 'Y', 'MT') ) & (df$REL_COV > 0.25)
-df$SV <- factor(df$CHROM, levels = c('0-2 Mbp', '2-10 Mbp', '10-50 Mbp', '50-250 Mbp'))
-df$SV[df$LENGTH < 250e6] <- '50-250 Mbp'
-df$SV[df$LENGTH < 50e6] <- '10-50 Mbp'
-df$SV[df$LENGTH < 10e6] <- '2-10 Mbp'
-df$SV[df$LENGTH <= 2e6] <- '0-2 Mbp'
+df_calls$SV <- factor(df_calls$CHROM, levels = c('0-2 Mbp', '2-10 Mbp', '10-50 Mbp', '50-250 Mbp'))
+df_calls$SV[df_calls$LENGTH <= 250e6] <- '50-250 Mbp'
+df_calls$SV[df_calls$LENGTH <= 50e6] <- '10-50 Mbp'
+df_calls$SV[df_calls$LENGTH <= 10e6] <- '2-10 Mbp'
+df_calls$SV[df_calls$LENGTH <= 2e6] <- '0-2 Mbp'
+df_calls$BDEV[is.na(df_calls$BDEV)] <- -.05
 
-bdev <- .3;
-df$BDEV[is.na(df$BDEV)] <- bdev
-cnf_dup <- 1 / (0.5 - bdev);
-cnf_del <- 1 / (0.5 + bdev);
-fs <- 16
-
-p1 <- ggplot(df[idx,], aes(x=BDEV, y=REL_COV, color=TYPE, shape=TYPE)) +
-  geom_hline(yintercept = c(1.0, 2.0, 3.0), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_dup), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_del), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_point(size=1, alpha=1/2) +
-  scale_color_manual('', breaks = c("CNN-LOH", "CNP Deletion", "CNP Duplication", "Deletion", "Duplication", "Undetermined"), values=c("green", "orange", "cyan", "red", "blue", "gray")) +
-  scale_shape_manual('', values=0:5) +
-  scale_x_continuous('Bdev', breaks = c(0, .1, .2, .3)) +
-  scale_y_log10('Rel_Cov', breaks = c(1, 2, 3, 4)) +
-  theme_bw(base_size = fs) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  facet_wrap(~SV)
-
-p2 <- ggplot(df[idx,], aes(x=BDEV, y=REL_COV, color=CHROM, shape=CHROM)) +
-  geom_hline(yintercept = c(1.0, 2.0, 3.0), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_dup), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_del), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_point(size=1, alpha=1/2) +
-  scale_color_discrete('Chromosome') +
-  scale_shape_manual('Chromosome', values=0:23) +
-  scale_x_continuous('Bdev', breaks = c(0, .1, .2, .3)) +
-  scale_y_log10('Rel_Cov', breaks = c(1, 2, 3, 4)) +
-  theme_bw(base_size = fs) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  facet_wrap(~SV)
-
-p3 <- ggplot(df[idx,], aes(x=BDEV, y=REL_COV, color=(NFLIPS!=-1), shape=(NFLIPS!=-1))) +
-  geom_hline(yintercept = c(1.0, 2.0, 3.0), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_dup), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_segment(aes(x = 0, y = 2.0, xend = bdev, yend = cnf_del), color = 'gray', size = .5, linetype = 'dashed') +
-  geom_point(size=1, alpha=1/2) +
-  scale_color_discrete('Model', labels = c('LRR+BAF', 'BAF+phase')) +
-  scale_shape_manual('Model', values=0:1, labels = c('LRR+BAF', 'BAF+phase')) +
-  scale_x_continuous('Bdev', breaks = c(0,.1,.2,.3)) +
-  scale_y_log10('Rel_Cov', breaks = c(1,2,3,4)) +
-  theme_bw(base_size = fs) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  facet_wrap(~SV)
-
-idx <- df$LENGTH > 2e6 | df$BDEV<.12 | df$REL_COV<2.5 # non-germline events
-df <- as.data.frame.matrix(table(df[idx, c('CHROM', 'TYPE')]))
-df$CHROM <- rownames(df)
-idx <- !( df$CHROM %in% c('X', 'Y', 'MT') )
-p4 <- ggplot(df[idx,], aes(x=Deletion, y=Duplication, color=CHROM, shape=CHROM, label=CHROM)) +
-  geom_point() +
-  geom_text(color = 'black', hjust=-.1, vjust=-.1) +
-  scale_color_discrete(guide = FALSE) +
-  scale_shape_manual(values=0:23, guide=FALSE) +
-  theme_bw(base_size = fs)
-
-p5 <- ggplot(df_stats, aes(x=X_NONPAR_ADJ_LRR_MEDIAN, y=X_NONPAR_NHETS, color=SEX)) +
-  geom_point(alpha=1/3) +
-  scale_x_continuous('X nonPAR median LRR (autosome corrected)') +
-  scale_y_continuous('X nonPAR number of heterozygous sites') +
-  scale_color_discrete(guide = FALSE) +
-  theme_bw(base_size = fs)
-p6 <- ggplot(df_stats, aes(x=X_NONPAR_ADJ_LRR_MEDIAN, y=Y_NONPAR_ADJ_LRR_MEDIAN, color=SEX)) +
-  geom_point(alpha=1/3) +
-  scale_x_continuous('X nonPAR median LRR (autosome corrected)') +
-  scale_y_continuous('Y nonPAR median LRR (autosome corrected)') +
-  scale_color_discrete(guide = FALSE) +
-  theme_bw(base_size = fs)
-p7 <- ggplot(df_stats, aes(x=X_NONPAR_ADJ_LRR_MEDIAN, y=MT_ADJ_LRR_MEDIAN, color=SEX)) +
-  geom_point(alpha=1/3) +
-  scale_x_continuous('X nonPAR median LRR (autosome corrected)') +
-  scale_y_continuous('MT median LRR (autosome corrected)') +
-  scale_color_discrete(guide = FALSE) +
-  theme_bw(base_size = fs)
-p8 <- ggplot(df_stats, aes(x=Y_NONPAR_ADJ_LRR_MEDIAN, y=MT_ADJ_LRR_MEDIAN, color=SEX)) +
-  geom_point(alpha=1/3) +
-  scale_x_continuous('Y nonPAR median LRR (autosome corrected)') +
-  scale_y_continuous('MT median LRR (autosome corrected)') +
-  scale_color_discrete(guide = FALSE) +
-  theme_bw(base_size = fs)
-if (model == 'wgs') {
-  p9 <- ggplot(df_stats, aes(x=BAF_CORR, y=BAF_CONC)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Beta-binomial correlation BAF') +
-    scale_y_continuous('BAF concordance') +
-    theme_bw(base_size = fs)
-  p10 <- ggplot(df_stats, aes(x=REL_ESS, y=COV_AUTO)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Relative coverage variance explained by GC') +
-    scale_y_continuous('GC-adjusted coverage auto-correlation') +
-    theme_bw(base_size = fs)
-  p11 <- ggplot(df_stats, aes(x=COV_SD, y=COV_AUTO)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Standard deviation coverage') +
-    scale_y_continuous('GC-adjusted coverage auto-correlation') +
-    theme_bw(base_size = fs)
+if (!is.null(args$pdf)) {
+  pdf(args$pdf)
 } else {
-  p9 <- ggplot(df_stats, aes(x=BAF_SD, y=BAF_CONC)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Standard deviation BAF') +
-    scale_y_continuous('BAF concordance') +
-    theme_bw(base_size = fs)
-  p10 <- ggplot(df_stats, aes(x=REL_ESS, y=LRR_AUTO)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Relative LRR variance explained by GC') +
-    scale_y_continuous('GC-adjusted LRR auto-correlation') +
-    theme_bw(base_size = fs)
-  p11 <- ggplot(df_stats, aes(x=LRR_SD, y=LRR_AUTO)) +
-    geom_point(alpha=1/3) +
-    scale_x_continuous('Standard deviation LRR') +
-    scale_y_continuous('GC-adjusted LRR auto-correlation') +
-    theme_bw(base_size = fs)
+  png(args$png, width = 7, height = 7, units = 'in', res = 150)
 }
 
-pdf(out_pdf)
-print(p1)
-print(p2)
-print(p3)
-print(p4)
-print(p5)
-print(p6)
-print(p7)
-print(p8)
-print(p9)
-print(p10)
-print(p11)
+idx <- !( df_calls$CHROM %in% c('X', 'Y', 'MT') )
+p <- ggplot(df_calls[idx,], aes(x=BDEV, y=REL_COV, color=TYPE, shape=TYPE)) +
+  geom_hline(yintercept = c(1.0, 2.0, 3.0), color = 'gray', size = .5, linetype = 'dashed') +
+  geom_segment(aes(x = 0.0, y = 2.0, xend = 1.0/6.0, yend = 3.0), color = 'gray', size = .5, linetype = 'dashed') +
+  geom_segment(aes(x = 0.0, y = 2.0, xend = 1.0/6.0, yend = 1.5), color = 'gray', size = .5, linetype = 'dashed') +
+  geom_point(size = 1, alpha = 1/2) +
+  scale_color_manual('', values = c('CNN-LOH' = 'green', 'CNP Deletion' = 'orange', 'CNP Duplication' = 'cyan', 'Deletion' = 'red', 'Duplication' = 'blue', 'Undetermined' = 'gray')) +
+  scale_shape_manual('', values = 0:5) +
+  theme_bw(base_size = args$fontsize) +
+  theme(strip.background = element_rect(color=NA, fill=NA), legend.position = 'bottom', legend.box = 'horizontal') +
+  facet_wrap(~SV)
+print(p + scale_x_continuous('BAF deviation (Bdev)', breaks = c(-0.05, 0.0, 0.05, 0.1, 0.15, 0.2)) + scale_y_continuous(expression(paste('Relative coverage (rescaled ', e^LRR, ')')), breaks = c(0, 1, 2, 3, 4)) + coord_cartesian(xlim = c(-0.05, 0.2), ylim = c(0, 4)))
+print(p + scale_x_continuous('BAF deviation (Bdev)', breaks = c(0.0, 0.01, 0.02, 0.03, 0.04, 0.05)) + scale_y_continuous(expression(paste('Relative coverage (rescaled ', e^LRR, ')')), breaks = c(1.8, 1.9, 2.0, 2.1, 2.2)) + coord_cartesian(xlim = c(0.0, 0.05), ylim = c(1.8, 2.2)))
+
+idx <- !( df_calls$CHROM %in% c('X', 'Y', 'MT') ) & !( df_calls$TYPE %in% c('CNP Deletion', 'CNP Duplication') ) & (df_calls$LENGTH > 2e6 | df_calls$BDEV<.12 | df_calls$REL_COV<2.5 & df_calls$REL_COV>1.5) # non-germline events
+p <- ggplot(df_calls[idx,], aes(x=CHROM, fill=TYPE)) +
+  geom_bar(stat = 'count', color = 'black') +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_manual('', values = c('CNN-LOH' = 'green', 'Deletion' = 'red', 'Duplication' = 'blue', 'Undetermined' = 'gray')) +
+  theme_bw(base_size = args$fontsize) +
+  theme(legend.position = 'bottom', legend.box = 'horizontal')
+print(p)
+
+p <- ggplot(df_stats, aes(x=X_NONPAR_ADJ_LRR_MEDIAN, y=X_NONPAR_NHETS, color=SEX)) +
+  geom_point(size = .5, alpha = 1/2) +
+  scale_x_continuous('X nonPAR median LRR (autosome corrected)') +
+  scale_y_continuous('X nonPAR number of heterozygous sites') +
+  scale_color_manual('', values = c('M' = 'blue', 'F' = 'orchid', 'U' = 'gray'), labels = c('M' = 'Male', 'F' = 'Female', 'U' = 'Undetermined')) +
+  theme_bw(base_size = args$fontsize) +
+  theme(legend.position = 'bottom', legend.box = 'horizontal')
+print(p)
+
+if ('LRR_AUTO' %in% colnames(df_stats)) { col_x <- 'LRR_AUTO'; lbl_x <- 'GC-adjusted LRR auto-correlation'
+} else if ('COV_AUTO' %in% colnames(df_stats)) { col_x <- 'COV_AUTO'; lbl_x <- 'GC-adjusted coverage auto-correlation' }
+p <- ggplot(df_stats, aes_string(x = col_x, y = 'BAF_AUTO', color = 'SEX')) +
+  geom_point(size = .5, alpha = 1/2) +
+  scale_x_continuous(lbl_x) +
+  scale_y_continuous('BAF auto-correlation') +
+  scale_color_manual('', values = c('M' = 'blue', 'F' = 'orchid', 'U' = 'gray'), labels = c('M' = 'Male', 'F' = 'Female', 'U' = 'Undetermined')) +
+  theme_bw(base_size = args$fontsize) +
+  theme(legend.position = 'bottom', legend.box = 'horizontal')
+print(p)
+
+if ('BAF_SD' %in% colnames(df_stats)) { col_x <- 'BAF_SD'; lbl_x <- 'Standard deviation BAF'
+} else if ('BAF_CORR' %in% colnames(df_stats)) { col_x <- 'BAF_CORR'; lbl_x <- 'Beta-binomial correlation BAF' }
+if ('LRR_SD' %in% colnames(df_stats)) { col_y <- 'LRR_SD'; lbl_y <- 'Standard deviation LRR'
+} else if ('COV_SD' %in% colnames(df_stats)) { col_y <- 'COV_SD'; lbl_y <- 'Standard deviation coverage' }
+p <- ggplot(df_stats, aes_string(x = col_x, y = col_y, color = 'SEX')) +
+  geom_point(size = .5, alpha = 1/2) +
+  scale_x_continuous(lbl_x) +
+  scale_y_continuous(lbl_y) +
+  scale_color_manual('', values = c('M' = 'blue', 'F' = 'orchid', 'U' = 'gray'), labels = c('M' = 'Male', 'F' = 'Female', 'U' = 'Undetermined')) +
+  theme_bw(base_size = args$fontsize) +
+  theme(legend.position = 'bottom', legend.box = 'horizontal')
+print(p)
+
 invisible(dev.off())
